a0fb71f9158b37d9d730a4167bcbbbd560a03856
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-27 04:37:35 +0000

    Setup redux store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from 'react';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const App = () => {
+  return <h1>App</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
