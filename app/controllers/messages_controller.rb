7d0d86544da15faf236a140bea872b6917c18a66
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-27 04:02:57 +0000

    Implement a messages_controller with random_greeting action and setup routes/ api endpoint

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,9 @@
+class MessagesController < ApplicationController
+  def random_greeting
+    @greeting = Message.order('RANDOM()').first
+    render json: { greeting: @greeting.greeting }
+  rescue StandardError => e
+    render json: { error: e.message, status: :unprocessable_entity }
+  end
+  end
+end
